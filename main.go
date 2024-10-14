6af7554290a9c9b56b77fa1c1a359a9e080891a3
shubham09anand <shubhamsheelam@gmail.com>
2024-10-14 17:53:00 +0530

    init

--- main.go
+++ main.go
@@ -22,18 +22,7 @@ func main() {
 
 func CORSMiddleware() gin.HandlerFunc {
 	return func(c *gin.Context) {
-		allowedOrigins := map[string]bool{
-			"http://insider.shubham09anand.in":     true,
-			"https://insider.shubham09anand.in":    true,
-			"http://apiinsider.shubham09anand.in":  true,
-			"https://apiinsider.shubham09anand.in": true,
-		}
-
-		origin := c.Request.Header.Get("Origin")
-		if allowedOrigins[origin] {
-			c.Writer.Header().Set("Access-Control-Allow-Origin", origin)
-		}
-
+		c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
 		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
 		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
 		c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT")
@@ -42,7 +31,6 @@ func CORSMiddleware() gin.HandlerFunc {
 			c.AbortWithStatus(204)
 			return
 		}
-
 		c.Next()
 	}
 }
